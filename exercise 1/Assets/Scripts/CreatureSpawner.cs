db6187d61c39e025bbf50ad0631643833e190f18
Jonathan Ciottone <jpc4483@rit.edu>
2023-09-11 16:59:13 -0400

    update

--- exercise 1/Assets/Scripts/CreatureSpawner.cs
+++ exercise 1/Assets/Scripts/CreatureSpawner.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CreatureSpawner : MonoBehaviour
 {
-
+    //Gameobject for prefab
     [SerializeField] GameObject creaturePrefab;
     
-
+    //the three spawn locations that the objects will be using
     [SerializeField] Vector3 spawnPositionOne = new Vector3();
     [SerializeField] Vector3 spawnPositionTwo = new Vector3();
     [SerializeField] Vector3 spawnPositionThree = new Vector3();
@@ -16,7 +16,7 @@ public class CreatureSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       //Instantiate all creatures at their spawn points
         Instantiate(creaturePrefab,spawnPositionOne,Quaternion.identity);
         Instantiate(creaturePrefab, spawnPositionTwo, Quaternion.identity);
         Instantiate(creaturePrefab, spawnPositionThree, Quaternion.identity);
