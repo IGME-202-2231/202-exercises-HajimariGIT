162d924a5ac9fc32645aef48cea6e7a16163904b
Jonathan Ciottone <jpc4483@rit.edu>
2023-10-02 19:13:39 -0400

    exercise 5 done

--- Exercise 4/Assets/Scripts/CollisionManager.cs
+++ Exercise 4/Assets/Scripts/CollisionManager.cs
@@ -6,39 +6,50 @@ using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
+    //controls collidables on screen and stores
     [SerializeField]
     List<SpriteInfo> collideables = new List<SpriteInfo>();
+    //contros what equation to use
     bool control=true;
+    //controls text
     public TextMesh collisionText;
     // Start is called before the first frame update
    
+    /// <summary>
+    /// controls current mode
+    /// </summary>
     public enum Mode
     {
         Sqaure,
         Circle
     }
    
-
+    //starting state
     Mode modeState=Mode.Sqaure;
    
     // Update is called once per frame
     void Update()
     {
+        //if the user presses right click 
         if (Input.GetMouseButtonDown(0))
         {
+            //change the state of game to opposite equation 
             StateChange();
           
         }
 
+        //check state/text and change accordingly
+
         CheckState();
 
-
+        //assign a staring value of false
 
         foreach (SpriteInfo sprite in collideables)
         {
             sprite.IsColliding = false;
         }
 
+        //check for collisions
         onTouch();
 
        
@@ -50,16 +61,18 @@ public class CollisionManager : MonoBehaviour
     {
 
 
-
+        //compare each object with each object 
         for (int i = 0; i < collideables.Count - 1; i++)
         {
             for (int j = i + 1; j < collideables.Count; j++)
             {
+                //store them
                 SpriteInfo spriteA = collideables[i];
                 SpriteInfo spriteB = collideables[j];
 
+                //assume they are not collding 
                 bool isColliding = false;
-
+                //if control is false
                 if (control == false)
                 {
                     isColliding = AABBCheck(spriteA, spriteB);
@@ -69,10 +82,11 @@ public class CollisionManager : MonoBehaviour
                    isColliding= Circle(spriteA, spriteB);
                 }
 
-
+                //otherwise 
 
                 if (isColliding)
                 {
+                    //set both to true 
                     spriteA.IsColliding = true;
                     spriteB.IsColliding = true;
                 }
@@ -80,7 +94,12 @@ public class CollisionManager : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// Equation for checking boxes
+    /// </summary>
+    /// <param name="spriteA"></param>
+    /// <param name="spriteB"></param>
+    /// <returns></returns>
     bool AABBCheck(SpriteInfo spriteA, SpriteInfo spriteB)
      {
       // Check for collisions
@@ -90,10 +109,12 @@ public class CollisionManager : MonoBehaviour
           spriteB.RectMin.y < spriteA.RectMax.y &&
           spriteB.RectMax.y > spriteA.RectMin.y)
       {
+            //there is one 
             return true;
       }
       else
       {
+            //there is not one
             return false;
       }
         
@@ -103,9 +124,11 @@ public class CollisionManager : MonoBehaviour
 
    bool Circle(SpriteInfo spriteA, SpriteInfo spriteB)
     {
+        //Pythagorean theorem
         double x= spriteB.center.x- spriteA.center.x;
         double y= spriteB.center.y -spriteA.center.y;
         double total =Math.Sqrt(x*x + y*y);
+        //check if they intersect
         if(total < spriteA.Radius + spriteB.Radius)
         {
             return true;
@@ -116,7 +139,10 @@ public class CollisionManager : MonoBehaviour
         }
     }
 
-   
+   /// <summary>
+   /// checks words and updates control to switch equations 
+   /// </summary>
+   /// <returns></returns>
 
     bool CheckState()
     {
@@ -133,6 +159,9 @@ public class CollisionManager : MonoBehaviour
         }
 
     }
+    /// <summary>
+    /// switches current state 
+    /// </summary>
 
     void StateChange()
     {
--- Exercise 4/Assets/Scripts/SpriteInfo.cs
+++ Exercise 4/Assets/Scripts/SpriteInfo.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class SpriteInfo : MonoBehaviour
 {
 
-    // Properties for min/max
+   //controls the render
     [SerializeField] SpriteRenderer renderer;
+    //collider value
     bool isColliding = false;
+    //size of rectangle
     [SerializeField] Vector2 ractSize = Vector2.one;
+    //refrebce if needed
     CollisionManager test = new CollisionManager();
 
+    //get radius 
     public float Radius
     {
         get { return renderer.bounds.size.x / 2 + 0.07f; }
     }
    
-    
+   /// <summary>
+   /// property for collide
+   /// </summary>
     public bool IsColliding
     {
         set { isColliding = value; }
     }
 
+    /// <summary>
+    /// property for min rectangle
+    /// </summary>
     public Vector3 RectMin
     {
         get { return renderer.bounds.min; }
     }
-
+    /// <summary>
+    /// property for mac rectangle
+    /// </summary>
     public Vector3 RectMax
     {
         get { return renderer.bounds.max; }
     }
-
+    /// <summary>
+    /// property for center of circle
+    /// </summary>
     public Vector3 center
     {
         get { return renderer.bounds.center; }
@@ -42,6 +55,7 @@ public class SpriteInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //changes color
       if(isColliding)
       {
             renderer.color = Color.red;
