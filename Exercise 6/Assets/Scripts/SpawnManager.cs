6e6417fb464d1ba7ce9f6736ce6d7d182c7d8282
Jonathan Ciottone <jpc4483@rit.edu>
2023-10-05 14:56:09 -0400

    update

--- Exercise 6/Assets/Scripts/SpawnManager.cs
+++ Exercise 6/Assets/Scripts/SpawnManager.cs
@@ -5,19 +5,82 @@ using UnityEngine;
 public class SpawnManager : Singleton<SpawnManager>
 {
 
-    public string name = "eric";
+    [SerializeField] SpriteRenderer animalPrefab;
+
+    [SerializeField] List<Sprite> animalImages = new List<Sprite>();
+
+    List<SpriteRenderer> spawnedAnimals = new List<SpriteRenderer> ();
+
+
+
+
+
     // (Optional) Prevent non-singleton constructor use.
     protected SpawnManager() { }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Spawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+
+
+    public  SpriteRenderer SpawnCreature()
+    {
+        return Instantiate(animalPrefab); 
+    }
+
+
+    public void Spawn()
+    {
+        DestroyAnimal();
+        for(int i = 0; i< 1000; ++i)
+        {
+            spawnedAnimals.Add(SpawnCreature());
+
+
+            //do random
+            spawnedAnimals[i].color = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+
+            //set position 
+            Vector2 spawnpos;
+            spawnpos.x = Random.Range(-4f, 4f);
+            spawnpos.y = Random.Range(-2f, 2f);
+
+            spawnedAnimals[i].transform.position = spawnpos;
+
+
+            float ranValue = Random.value;
+
+            if(ranValue < 0.6f)
+            {
+                spawnedAnimals[i].sprite = animalImages[0];
+            }
+            else if(ranValue < 0.84)
+            {
+                spawnedAnimals[i].sprite = animalImages[2];
+            }
+            else
+            {
+                spawnedAnimals[i].sprite = animalImages[3];
+            }
+        }
+    }
+
+    public void DestroyAnimal()
+    {
+        foreach(SpriteRenderer animal in spawnedAnimals)
+        {
+            Destroy(animal.gameObject);
+        }
+
+        spawnedAnimals.Clear();
+
     }
 }
