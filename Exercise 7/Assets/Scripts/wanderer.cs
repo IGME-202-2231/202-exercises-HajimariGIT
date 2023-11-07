4b1800aeca1eb965f1f9f494b4e7e366f8e42d31
Jonathan Ciottone <jpc4483@rit.edu>
2023-11-07 14:54:52 -0500

    done

--- Exercise 7/Assets/Scripts/wanderer.cs
+++ Exercise 7/Assets/Scripts/wanderer.cs
@@ -8,16 +8,7 @@ public class wanderer : agent
     // Start is called before the first frame update
     public float time = 1f;
     public float radius = 1f;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     protected override void CalcSteeringForce()
     {
