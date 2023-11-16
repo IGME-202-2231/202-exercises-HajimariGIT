34ecd4471dc12947698df60d1ff465ab5c3d1a4e
Jonathan Ciottone <jpc4483@rit.edu>
2023-11-16 14:36:55 -0500

    push

--- Exercise 7/Assets/Scripts/Agent Manager.cs
+++ Exercise 7/Assets/Scripts/Agent Manager.cs
@@ -8,6 +8,10 @@ public class AgentManager : MonoBehaviour
     public List<agent> agents;
 
     // Start is called before the first frame update
+
+    public float CountTime = 5;
+
+    public agent ItPlayer;
     void Start()
     {
         
@@ -17,5 +21,28 @@ public class AgentManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public agent FlockClosestPlayer()
+    {
+        
+        float min = Mathf.Infinity, dis;
+        agent nearest = null;
+
+
+        foreach(agent player in agents)
+        {
+            if(player != ItPlayer)
+            {
+                dis = Vector2.Distance(ItPlayer.transform.position, player.transform.position);
+
+                if(dis < min)
+                {
+                    nearest = player;
+                    min = dis;
+                }
+            }
+        }
     }
 }
